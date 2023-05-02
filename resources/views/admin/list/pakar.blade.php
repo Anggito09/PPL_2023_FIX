86a2a0a639ef0aa6b6d1588f03b534439e68e5d9
Anggito09 <114100817+Anggito09@users.noreply.github.com>
2023-05-02 23:34:19 +0700

    Update pakar.blade.php

--- resources/views/admin/list/pakar.blade.php
+++ resources/views/admin/list/pakar.blade.php
@@ -20,19 +20,20 @@
                 </div>
             </div>
             <div class="bg-secondary w-3/4 flex flex-col gap-4 h-[80vh] overflow-y-scroll p-8 rounded-xl">
-                <h1 class="font-bold text-xl">Akun Pakar</h1>
+                <h1 class="font-bold text-xl">Akun Petani</h1>
                 <div>
-                    @foreach($pakars as $pakar)
+                    @foreach($petanis as $petani)
                         <div class="flex justify-between bg-primary px-4 py-2 rounded-xl">
                             <div class="flex items-center gap-4">
                                 <img src="/images/icon4.png" alt="">
                                 <div>
-                                    <h2 class="font-bold text-xl">{{$pakar->name}}</h2>
-                                    <h3>Pakar</h3>
+                                    <h2 class="font-bold text-xl">{{$petani->name}}</h2>
+                                    <h3>Petani</h3>
                                 </div>
                             </div>
-                            <div class="flex flex-col items-end justify-end">
-                                <a href="/editpakar/{{$pakar->id}}" class="btn btn-primary px-12">Edit</a>
+                            <div class="flex flex-col items-end justify-between">
+                                <h3>Reguler</h3>
+                                <a href="/editpetani/{{$petani->id}}" class="btn btn-primary px-12">Edit</a>
                             </div>
                         </div>
                     @endforeach
