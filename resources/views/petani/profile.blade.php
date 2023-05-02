742f2d81a296adfe28cc7c32e03a1cb274416b59
Anggito09 <114100817+Anggito09@users.noreply.github.com>
2023-05-02 23:37:23 +0700

    Update profile.blade.php

--- resources/views/petani/profile.blade.php
+++ resources/views/petani/profile.blade.php
@@ -10,7 +10,7 @@
                     <h3 class="capitalize">{{auth()->user()->role->role_name}}</h3>
                 </div>
                 <div class="flex flex-col gap-2">
-                    <a href="/listbantutani" class="font-medium text-lg bg-secondary drop-shadow-lg py-1 px-8">Data Transaksi Akun</a>
+                    <a href="/listbantutani" class="font-medium text-lg bg-secondary drop-shadow-lg py-1 px-8">Data  Investasi</a>
                     <a href="/listinvestasi" class="font-medium text-lg bg-secondary drop-shadow-lg py-1 px-8">Data Transaksi Investasi</a>
                 </div>
             </div>
@@ -31,7 +31,7 @@
                             <p>{{auth()->user()->email}}</p>
                         </div>
                         <div>
-                            <h2 class="font-medium text-lg">No Telepon</h2>
+                            <h2 class="font-medium text-lg">Nomor handphone</h2>
                             <p>{{auth()->user()->phone}}</p>
                         </div>
                     </div>
@@ -45,7 +45,7 @@
                             <p>{{auth()->user()->address}}</p>
                         </div>
                         <div>
-                            <h2 class="font-medium text-lg">No Rekening</h2>
+                            <h2 class="font-medium text-lg">Nomor Rekening</h2>
                             <p>{{auth()->user()->rekening}}</p>
                         </div>
                     </div>
